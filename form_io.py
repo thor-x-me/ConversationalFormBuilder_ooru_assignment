4a89c5185fc3d5ba469fdce1f19f010af4cf2391
Abhishek Verma <abhishekverm2018@gmail.com>
2026-05-05 05:43:22 +0530

    added method for versioned form operation

--- form_io.py
+++ form_io.py
@@ -1,4 +1,5 @@
 import requests
+import json
 BASE_URL = "http://localhost:3001"
 
 
@@ -63,3 +64,71 @@ def get_form_data(form_id: str):
 
     form_data = response.json()
     return form_data
+
+def get_versioned_form_data():
+    with open("form_data.json") as f:
+        data = json.load(f)
+    return data
+
+def add_versioned_form(form_id, version, new_form):
+    with open("form_data.json") as f:
+        data = json.load(f)
+        
+    data = data if data else []
+    new_form_body = {form_id: {version: new_form}}
+    data.append(new_form_body)
+
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=4)
+    return new_form_body
+
+def update_versioned_form(form_id, version, updated_form):
+    with open("form_data.json") as f:
+        data = json.load(f)
+
+    index = next((i for i, item in enumerate(data) if item["id"] == form_id), None)
+    if index is not None:
+        data[index][version] = updated_form
+
+        with open("data.json", "w") as f:
+            json.dump(data, f, indent=4)
+        return True
+    else:
+        return False
+
+def get_versioned_form(form_id, version):
+    with open("form_data.json") as f:
+        data = json.load(f)
+
+    form = next((item for item in data if item["id"] == form_id), None)
+
+    if form:
+        versioned_form = form.get(version)
+    else:
+        return {}
+    return versioned_form
+
+def update_form(form_id: str, modified_form: dict):
+    """
+    Update a form's  via API and return the result.
+    """
+    print("Function update_form called ...")
+    EMAIL = "admin@example.com"
+    PASSWORD = "CHANGEME"
+    token = authenticate(EMAIL, PASSWORD)
+
+    headers = {
+        "Content-Type": "application/json",
+        "x-jwt-token": token,
+    }
+    update_response = requests.put(
+        f"{BASE_URL}/form/{form_id}",
+        json=modified_form,
+        headers=headers,
+    )
+
+    if update_response.status_code == 200:
+        updated = update_response.json()
+        return updated
+    else:
+        raise RuntimeError(f"Update failed: {update_response.status_code} - {update_response.text}")

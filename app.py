f6b6afe16cd06a4dae6d2132707de647ebf4c747
Abhishek Verma <abhishekverm2018@gmail.com>
2026-05-05 18:12:51 +0530

    fixed /get_all_form_details to call actual function, added context of form in chat

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
 from fastapi import FastAPI, File, Form, UploadFile
-import form_io
+import traceback
 from agents import agent
-# from agents import stream_agent
 from fastapi.responses import StreamingResponse
 import base64
 from langchain_core.messages import HumanMessage
 import json
 from form_io import get_form_data, get_versioned_form, update_form, add_versioned_form, update_versioned_form, get_versioned_form_data
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/get_all_form_details')
 def get_all_form_details():
     # return form_io.get_created_forms()
-    return {"data": get_versioned_form_data}
+    return {"data": get_versioned_form_data()}
 
 @app.post("/chat")
 async def chat(
-    version: str | None = None,
-    form_id: str | None = None,
+    version: int | None = Form(default=None),
+    form_id: str | None = Form(default=None),
     message: str = Form(...),
     images: list[UploadFile] = File(default=[])
 ):
-    if version:
+    if version is not None:
         #loading the version of form requested by user
         old_form = get_versioned_form(form_id, version)
         update_form(form_id, old_form)
+        message = f"This request is for form if: {form_id}\n" + message
 
     content_list = [{"type": "text", "text": message}]
     
@@ -69,7 +78,7 @@ async def _generate(messages, version, form_id):
             try:
                 new_form = json.loads(output.content)
                 if tool_name == "create_form":
-                    new_form_body = add_versioned_form(new_form._id, 0, new_form)
+                    new_form_body = add_versioned_form(new_form.get("_id"), 1, new_form)
                 elif tool_name == "update_form":
                     new_form_body = update_versioned_form(form_id, version+1, new_form)
 

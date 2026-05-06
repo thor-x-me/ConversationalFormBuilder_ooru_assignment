b0c496244caeda6cbc676c4c7ed41f3c9a2f49a7
Abhishek Verma <abhishekverm2018@gmail.com>
2026-05-06 19:06:30 +0530

    form related operations

---
+++ form/form.py
@@ -0,0 +1,120 @@
+import requests
+import json
+from auth.auth import authenticate
+
+BASE_URL = "http://localhost:3001"
+EMAIL = "admin@example.com"
+PASSWORD = "CHANGEME"
+FORM_VERSIONED = "resources/form_data.json"
+
+def get_versioned_form_data():
+    with open(FORM_VERSIONED) as f:
+        data = json.load(f)
+    return data
+
+def add_versioned_form(form_id, version, new_form):
+
+    try:
+        with open(FORM_VERSIONED) as f:
+            data = json.load(f)
+    except json.decoder.JSONDecodeError as e:
+        data = []
+
+    new_form_body = {form_id: {version: new_form}}
+    data.append(new_form_body)
+
+    with open(FORM_VERSIONED, "w") as f:
+        json.dump(data, f, indent=4)
+    return new_form_body
+
+def update_versioned_form(form_id, version, updated_form):
+    with open(FORM_VERSIONED) as f:
+        data = json.load(f)
+
+    index = next((i for i, item in enumerate(data) if form_id in item.keys()), None)
+    if index is not None:
+        data[index][form_id][version] = updated_form
+
+        with open(FORM_VERSIONED, "w") as f:
+            json.dump(data, f, indent=4)
+        return True
+    else:
+        return False
+
+def get_versioned_form(form_id, version):
+    with open(FORM_VERSIONED) as f:
+        data = json.load(f)
+
+    form = next((item for item in data if form_id in item.keys()), None)
+
+    if form:
+        versioned_form = form.get(version)
+    else:
+        return {}
+    return versioned_form
+
+
+def update_form(form_id: str, modified_form: dict):
+    """
+    Update a form's  via API and return the result.
+    """
+
+    token = authenticate(EMAIL, PASSWORD)
+    headers = {
+        "Content-Type": "application/json",
+        "x-jwt-token": token,
+    }
+    update_response = requests.put(
+        f"{BASE_URL}/form/{form_id}",
+        json=modified_form,
+        headers=headers,
+    )
+
+    if update_response.status_code == 200:
+        updated = update_response.json()
+        return updated
+    else:
+        raise RuntimeError(f"Update failed: {update_response.status_code} - {update_response.text}")
+
+
+
+def get_created_forms():
+    """
+    This endpoint lists all forms within the project.
+    """
+    token = authenticate(EMAIL, PASSWORD)
+    url = f"{BASE_URL}/form"
+    headers = {
+        "Content-Type": "application/json",
+        "x-jwt-token": token,
+    }
+    
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise RuntimeError(f"Failed to fetch forms: {response.status_code} {response.text}")
+
+    form_list = response.json()
+    return form_list
+
+
+def get_form_data(form_id: str):
+    """Retrieve metadata and configuration for a specific form from Form.io.
+
+    Args:
+        form_id (str): Unique identifier of the form.
+
+    """
+
+    token = authenticate(EMAIL, PASSWORD)
+    headers = {
+        "Content-Type": "application/json",
+        "x-jwt-token": token,
+    }
+
+    response = requests.get(f"{BASE_URL}/form/{form_id}", headers=headers)
+    if response.status_code != 200:
+        raise response.text
+
+    form_data = response.json()
+    return form_data
+
